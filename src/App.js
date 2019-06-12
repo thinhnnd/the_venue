91096ca2202596b6a1037f1bbf462e2cc79253c5
thinhnnd <16521179@gm.uit.edu.vn>
2019-06-12 16:44:17 +0700

    venueNfo: finished

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header'
 
 import Featured from './components/featured'
+import VenueNfo from './components/venueNfo';
 
 function App() {
   return (
     <div className="App" style={{ height: "1500px", background:'cyan'}}>
       <Header />
       <Featured />
+      <VenueNfo />
     </div>
   );
 }
