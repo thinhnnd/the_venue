4c7b4063a58fabe358b7e2bc8ddb7024ea9810bc
thinhnnd <16521179@gm.uit.edu.vn>
2019-06-12 15:00:18 +0700

    featured: done carrousel

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header'
 
+import Featured from './components/featured'
+
 function App() {
   return (
     <div className="App" style={{ height: "1500px", background:'cyan'}}>
       <Header />
+      <Featured />
     </div>
   );
 }
